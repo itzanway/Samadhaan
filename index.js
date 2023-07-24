c6d09af92efafecdee2e6861b03448302afd75d1
Anway Durge <124391429+itzanway@users.noreply.github.com>
2023-07-25 01:10:44 +0530

    img added

--- index.js
+++ index.js
@@ -24,3 +24,18 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+// search bar
+const f = document.getElementById('form');
+      const q = document.getElementById('query');
+      const google = 'https://www.google.com/search?q=site%3A+';
+      const site = 'pagedart.com';
+
+      function submitted(event) {
+        event.preventDefault();
+        const url = google + site + '+' + q.value;
+        const win = window.open(url, '_blank');
+        win.focus();
+      }
+
+      f.addEventListener('submit', submitted);
